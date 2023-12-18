b9e461de6eb5c9546119965d4c6d249b2954b3c4
Digitalship <83201044+digital-10@users.noreply.github.com>
2023-12-18 15:37:34 +0900

    Update MetaProfilingController.java

--- src/main/java/kr/co/digitalship/dprep/custom/controller/MetaProfilingController.java
+++ src/main/java/kr/co/digitalship/dprep/custom/controller/MetaProfilingController.java
@@ -1,4 +1,4 @@
-;s;spackage kr.co.d;s;s;sship.dprep.custom.controller;
+;s;s;spackage kr.co.d;s;s;sship.dprep.custom.controller;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
